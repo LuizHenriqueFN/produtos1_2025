d5493d028870af0e3f771a3e9003c7c15ff59518
LuizHenriqueFN <luizhenriqueneves81@gmail.com>
2025-04-01 14:37:24 -0300

    Criação inicial do projeto

---
+++ src/main/java/com/example/produtos1_2025/Produtos12025Application.java
@@ -0,0 +1,13 @@
+package com.example.produtos1_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Produtos12025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Produtos12025Application.class, args);
+    }
+
+}
